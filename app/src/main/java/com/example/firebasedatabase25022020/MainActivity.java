4e0e72c2d9bd052394f902b43d4940eabfae1f5e
phatdroid94 <phatdroid94@gmail.com>
2020-06-09 20:31:22 +0700

    object

--- app/src/main/java/com/example/firebasedatabase25022020/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase25022020/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.firebasedatabase25022020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1 : Gui du lieu dang string
+        // Tao ra 1 node
+//        myRef.child().child("").setValue();
+        // Tao ra 1 child
+//        myRef.child("chuoi").setValue("Hello").addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void aVoid) {
+//                Toast.makeText(MainActivity.this, "Thanh cong", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        // 2 : Object
+
+
     }
 }
